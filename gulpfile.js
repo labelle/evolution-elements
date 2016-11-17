8d352a9228ccb831b6fec1f14585c9d8204a05f2
jdickan <jasen@humanig.com>
2016-11-16 21:35:42 -0500

    revert to v1.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ Steps:
 1. Install gulp globally:
 npm install --global gulp
 2. Type the following after navigating in your project folder:
-npm install gulp gulp-util gulp-sass gulp-uglify gulp-rename gulp-clean-css gulp-notify gulp-concat gulp-plumber browser-sync --save-dev
+npm install gulp gulp-util gulp-sass gulp-uglify gulp-rename gulp-minify-css gulp-notify gulp-concat gulp-plumber browser-sync --save-dev
 3. Move this file in your project folder
 4. Setup your vhosts or just use static server (see 'Prepare Browser-sync for localhost' below)
-5. Type 'Gulp' and start developing
+5. Type 'Gulp' and ster developing
 */
 
 /* Needed gulp config */
@@ -18,7 +18,7 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var notify = require('gulp-notify');
-var cleanCSS = require('gulp-clean-css');
+var minifycss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
@@ -29,8 +29,8 @@ var neat = require('node-neat').includePaths;
 gulp.task('scripts', function() {
   return gulp.src([
     /* Add your JS files here, they will be combined in this order */
-    'docs/js/vendor/jquery-1.11.1.js',
-    'docs/js/app.js'
+    'js/vendor/jquery-1.11.1.js',
+    'js/app.js'
     ])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('js'))
@@ -46,10 +46,10 @@ gulp.task('sass', function () {
     .pipe(sass({
         includePaths: ['scss'].concat(neat)
     }))
-    .pipe(gulp.dest('docs/css'))
+    .pipe(gulp.dest('css'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('docs/css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
     /* Reload the browser CSS after every change */
     .pipe(reload({stream:true}));
 });
@@ -61,7 +61,7 @@ gulp.task('bs-reload', function () {
 
 /* Prepare Browser-sync for localhost */
 gulp.task('browser-sync', function() {
-    browserSync.init(['docs/css/*.css', 'docs/js/*.js'], {
+    browserSync.init(['css/*.css', 'js/*.js'], {
         /*
         I like to use a vhost, WAMP guide: https://www.kristengrote.com/blog/articles/how-to-set-up-virtual-hosts-using-wamp, XAMP guide: http://sawmac.com/xampp/virtualhosts/
         */
@@ -69,7 +69,7 @@ gulp.task('browser-sync', function() {
         /* For a static server you would use this: */
         
         server: {
-            baseDir: './docs/'
+            baseDir: './'
         }
         
     });
@@ -80,7 +80,7 @@ gulp.task('default', ['sass', 'browser-sync'], function () {
     /* Watch scss, run the sass task on change. */
     gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass'])
     /* Watch app.js file, run the scripts task on change. */
-    gulp.watch(['docs/js/app.js'], ['scripts'])
+    gulp.watch(['js/app.js'], ['scripts'])
     /* Watch .html files, run the bs-reload task on change. */
-    gulp.watch(['docs/*.html'], ['bs-reload']);
+    gulp.watch(['*.html'], ['bs-reload']);
 });
